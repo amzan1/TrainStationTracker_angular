1cc73c9f376a13ab08501a7ebd8ba79523979a41
Muh-Hassk <Muh.alkhuzaee@gmail.com>
2024-07-23 23:46:58 +0300

    Fixed Put Needs Some enhancments.

--- src/app/admin/trips/trips.component.ts
+++ src/app/admin/trips/trips.component.ts
@@ -9,45 +9,42 @@ import { AdminService, Trip } from 'src/app/Services/admin.service';
   styleUrls: ['./trips.component.css']
 })
 export class TripsComponent implements OnInit {
-  constructor(private AdminService: AdminService, public dialog: MatDialog) { }
-
+  constructor(private AdminService: AdminService,public dialog: MatDialog) { }
   @ViewChild('deleteDialog') CallDeleteDialog!: TemplateRef<any>;
   @ViewChild('updateDialog') CallUpdateDialog!: TemplateRef<any>;
-
   tripSearch: string = '';
   PrevtripSearch: string = '';
-  pData: any;
+  pData:any;
   updateForm: FormGroup = new FormGroup({
-    tripid: new FormControl('', [Validators.required]),
-    originstationid: new FormControl('', [Validators.required]),
-    destinationstationid: new FormControl('', [Validators.required]),
-    departuretime: new FormControl('', [Validators.required]),
-    durationtime: new FormControl('', [Validators.required]),
-    availableseats: new FormControl('', [Validators.required]),
-    createdat: new FormControl('', [Validators.required]),
-    price: new FormControl('', [Validators.required]),
-  });
+    tripid: new FormControl('', [Validators.required]),  // Added stationid control
+    originstationid : new FormControl('', [Validators.required]),
+    destinationstationid : new FormControl('', [Validators.required]),
+    departuretime : new FormControl('', [Validators.required]),
+    duratointime : new FormControl('', [Validators.required]),
+    availableseats : new FormControl('', [Validators.required]),
+    createdat : new FormControl('', [Validators.required]),
+    price : new FormControl('', [Validators.required]),
+      })
 
   PrevTrips: Trip[] = [];
   Upcoming: Trip[] = [];
   stations: { [key: number]: string } = {};
-
   ngOnInit(): void {
     this.AdminService.getTrips().subscribe(data => {
       this.Upcoming = data.filter(trip => new Date(trip.departuretime) > new Date());
       this.PrevTrips = data.filter(trip => new Date(trip.departuretime) < new Date());
       console.log(this.Upcoming);
       console.log(this.PrevTrips);
+      
+      
     });
-  }
-
+  }    
   getStationName(id: number): string {
     return this.stations[id] || 'Unknown';
   }
-
-  openUpdateDialog(Trip: any) {
+  openUpdateDailog(Trip: any) {
     console.log('Trip:', Trip);
-
+    
     const dialogResult = this.dialog.open(this.CallUpdateDialog);
     this.pData = Trip;
     console.log("PData:", Trip);
@@ -56,18 +53,15 @@ export class TripsComponent implements OnInit {
     dialogResult.afterOpened().subscribe(() => {
       this.updateForm.controls['tripid'].setValue(this.pData.tripid);
       this.updateForm.controls['createdat'].setValue(this.pData.createdat);
-      this.updateForm.patchValue(this.pData); // Patch all values
       console.log("Trip ID set to:", this.pData.tripid);
     });
   }
 
   updateTrip() {
-    if (this.updateForm.valid) {
-      const updatedTrip = this.updateForm.value;
-      console.log('Updated Trip:', updatedTrip);
-      // Call your update service here with updatedTrip
-    }
+    console.log(this.updateForm.value);
+    
   }
+
   openDeleteDialog(id: number) {
     const dialogResult =
       this.dialog.open(this.CallDeleteDialog);
