cf5020346c75aa6403122d28c57bc443d8ef9c35
Muh-Hassk <Muh.alkhuzaee@gmail.com>
2024-08-20 05:48:18 +0300

    Finally Final Version.

--- src/app/user/landing-p/landing-p.component.ts
+++ src/app/user/landing-p/landing-p.component.ts
@@ -1,67 +1,71 @@
-import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AdminService } from 'src/app/Services/admin.service';
-import * as $ from 'jquery'; // Import jQuery
-import 'owl.carousel'; // Import Owl Carousel
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-landing-p',
   templateUrl: './landing-p.component.html',
   styleUrls: ['./landing-p.component.css']
 })
-export class LandingPComponent implements OnInit, OnDestroy, AfterViewInit {
+export class LandingPComponent implements OnInit, OnDestroy {
   testimonials: any[] = [];
+  currentTestimonials: any[] = [];
   currentIndex = 0;
-  sliderInterval: any;
-constructor(private adminService:AdminService) {}
-homepage:any
-stations:any
-searchQuery='';
+  displayCount = 3; // Number of testimonials to display at a time
+  homepage: any;
+  stations: any;
+  searchQuery = '';
+  private subscription: Subscription = new Subscription();
+
+  constructor(private adminService: AdminService) {}
+
   ngOnInit(): void {
-    this.adminService.getApprovedTestimonials().subscribe((res => {
+    this.loadTestimonials();
+    this.loadHomeContent();
+    this.loadAllTrainStations();
+  }
+
+  private loadTestimonials() {
+    this.adminService.getApprovedTestimonials().subscribe(res => {
       this.testimonials = res;
-      console.log(this.testimonials);
-      
-    }))
+      this.updateCurrentTestimonials();
+      this.startTestimonialRotation();
+    });
+  }
+
+  private loadHomeContent() {
     this.adminService.getHomeContent().subscribe(data => {
       if (data && data.length > 0) {
         this.homepage = data[0]; // Assuming the API returns an array with one item
       }
     });
-    this.adminService.getAllTrainStation().subscribe((res => {
-      this.stations = res
-    }));
-    this.initializeCarousel();
+  }
 
-  }
-  ngAfterViewInit() {
-    this.initializeCarousel();
-  }
-  initializeCarousel() {
-    ($ as any)('#testimonial-slider').owlCarousel({
-      items: 3,                // Display 3 items at a time
-      loop: true,              // Infinite loop
-      margin: 10,              // Margin between items
-      autoplay: true,         // Enable autoplay
-      autoplayTimeout: 1,  // Change slide every 5 seconds
-      autoplayHoverPause: true, // Pause on hover
-      nav: true,               // Show navigation arrows
-      dots: false,             // Hide pagination dots
-      responsive: {
-        0: {
-          items: 1 // Display 1 item on extra small screens
-        },
-        600: {
-          items: 2 // Display 2 items on small screens
-        },
-        1000: {
-          items: 3 // Display 3 items on medium and large screens
-        }
-      }
+  private loadAllTrainStations() {
+    this.adminService.getAllTrainStation().subscribe(res => {
+      this.stations = res;
     });
   }
-  
+
+  private updateCurrentTestimonials() {
+    this.currentTestimonials = this.testimonials.slice(this.currentIndex, this.currentIndex + this.displayCount);
+  }
+
+  private startTestimonialRotation() {
+    this.subscription.add(
+      interval(3 * 1000).subscribe(() => { // Rotate every 10 seconds
+        this.currentIndex += this.displayCount;
+        if (this.currentIndex >= this.testimonials.length) {
+          this.currentIndex = 0; // Loop back to the beginning
+        }
+        this.updateCurrentTestimonials();
+      })
+    );
+  }
+
   ngOnDestroy() {
-    // Cleanup Owl Carousel if necessary
-    $('.owl-carousel').trigger('destroy.owl.carousel');
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
